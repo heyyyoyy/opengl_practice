6104e9455a5e122229e621bb7e8390d8de420ed9
Sergey <heyyyoyy@gmail.com>
2022-10-06 21:36:15 +0300

    textures combined

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::ffi::CString;
+use std::{ffi::CString, path::Path};
 use std::mem;
 use std::os::raw::c_void;
 use std::sync::mpsc::Receiver;
+use std::ptr;
+
 use glfw::{Action, Context, Key};
 use gl::types::*;
-use std::ptr;
+use image;
 
 
 fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
@@ -27,21 +29,42 @@ fn main() {
     gl::load_with(|s| window.get_proc_address(s) as *const _);
     gl::Viewport::load_with(|s| window.get_proc_address(s) as *const _);
 
-    let (shader_program, vao) = unsafe {
+    let vao = unsafe {
         const VERTEX_SHADER_SOURCE: &str = r#"
             #version 330 core
             layout (location = 0) in vec3 position;
+            layout (location = 1) in vec3 color;
+            layout (location = 2) in vec2 TexCoord;
+
+            out vec3 our_color;
+            out vec2 tex_coord;
+
             void main()
             {
                 gl_Position = vec4(position.x, position.y, position.z, 1.0);
+                our_color = color;
+                tex_coord = TexCoord;
             }
         "#;
         const VERTEX_SHADER_FRAGMENT: &str = r#"
             #version 330 core
+            
+            in vec3 our_color;
+            in vec2 tex_coord;
+
             out vec4 color;
+
+            uniform sampler2D our_texture1;
+            uniform sampler2D our_texture2;
+
+
             void main()
             {
-                color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+                color = mix(
+                    texture(our_texture1, tex_coord), 
+                    texture(our_texture2, vec2(tex_coord.x, 1.0f - tex_coord.y)) * vec4(our_color, 1.0f), 
+                    0.5
+                );
             }
         "#;
         let c_vertex_shader_source = CString::new(VERTEX_SHADER_SOURCE.as_bytes()).unwrap();
@@ -107,14 +130,15 @@ fn main() {
         gl::DeleteShader(vertex_shader);
         gl::DeleteShader(fragment_shader);
 
-        type Vertex = [GLfloat; 3];
+        type Vertex = [GLfloat; 8];
         type Indexes = [u32; 3];
 
         let vertices: [Vertex; 4] = [
-            [0.5,  0.5, 0.0],  // Верхний правый угол
-            [0.5, -0.5, 0.0],  // Нижний правый угол
-            [-0.5, -0.5, 0.0],  // Нижний левый угол
-            [-0.5,  0.5, 0.0]   // Верхний левый угол
+             // Позициии       // Цвета         // Текстуры
+            [0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0],  // Верхний правый угол
+            [0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 0.0],  // Нижний правый угол
+            [-0.5, -0.5, 0.0,  0.0, 0.0, 1.0,   0.0, 0.0],  // Нижний левый угол
+            [-0.5,  0.5, 0.0,  1.0, 1.0, 0.0,   0.0, 1.0 ]   // Верхний левый угол
         ];
         let indices: [Indexes; 2] = [
           [0, 1, 3],
@@ -143,6 +167,61 @@ fn main() {
             gl::STATIC_DRAW
         );
 
+        let (mut texture1, mut texture2) = (0, 0);
+
+        // our_texture1
+        gl::GenTextures(1, &mut texture1);
+        gl::BindTexture(gl::TEXTURE_2D, texture1);
+
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+
+        let img = image::open(Path::new("assets/1.jpg")).unwrap();
+        let data = img.to_rgb8().into_raw();
+        gl::TexImage2D(
+            gl::TEXTURE_2D, 
+            0, 
+            gl::RGB as i32, 
+            img.width() as i32, 
+            img.height() as i32, 
+            0, 
+            gl::RGB, 
+            gl::UNSIGNED_BYTE, 
+            data.as_ptr() as *const c_void
+        );
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+        gl::BindTexture(gl::TEXTURE_2D, 0);
+
+        // our_texture2
+        gl::GenTextures(1, &mut texture2);
+        gl::BindTexture(gl::TEXTURE_2D, texture2);
+
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+
+        let img = image::open(Path::new("assets/2.png")).unwrap();
+        let data = img.to_rgb8().into_raw();
+        gl::TexImage2D(
+            gl::TEXTURE_2D, 
+            0, 
+            gl::RGB as i32, 
+            img.width() as i32, 
+            img.height() as i32, 
+            0, 
+            gl::RGB, 
+            gl::UNSIGNED_BYTE, 
+            data.as_ptr() as *const c_void
+        );
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+        gl::BindTexture(gl::TEXTURE_2D, 0);
+
+        // Позиции
         gl::VertexAttribPointer(
             0,
             3,
@@ -152,12 +231,43 @@ fn main() {
             ptr::null()
         );
         gl::EnableVertexAttribArray(0);
+        // Цвета
+        gl::VertexAttribPointer(
+            1,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            mem::size_of::<Vertex>().try_into().unwrap(),
+            mem::size_of::<[GLfloat; 3]>() as *const c_void
+        );
+        gl::EnableVertexAttribArray(1);
+        // Текстуры
+        gl::VertexAttribPointer(
+            2,
+            2,
+            gl::FLOAT,
+            gl::FALSE,
+            mem::size_of::<Vertex>().try_into().unwrap(),
+            mem::size_of::<[GLfloat; 6]>() as *const c_void
+        );
+        gl::EnableVertexAttribArray(2);
 
         gl::BindBuffer(gl::ARRAY_BUFFER, 0);
         gl::BindVertexArray(0);
 
+        gl::UseProgram(shader_program);
+
+        gl::ActiveTexture(gl::TEXTURE0);
+        gl::BindTexture(gl::TEXTURE_2D, texture1);
+        let our_texture1 = CString::new("our_texture1".as_bytes()).unwrap();
+        gl::Uniform1i(gl::GetUniformLocation(shader_program, our_texture1.as_ptr()), 0);
+        gl::ActiveTexture(gl::TEXTURE1);
+        gl::BindTexture(gl::TEXTURE_2D, texture2);
+        let our_texture2 = CString::new("our_texture2".as_bytes()).unwrap();
+        gl::Uniform1i(gl::GetUniformLocation(shader_program, our_texture2.as_ptr()), 1);  
+
         // gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
-        (shader_program, vao)
+        vao
     };
 
 
@@ -169,7 +279,6 @@ fn main() {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
-            gl::UseProgram(shader_program);
             gl::BindVertexArray(vao);
             // gl::DrawArrays(gl::TRIANGLES, 0, 3);
             gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
